c87d6272a6de2da7bbfab43a06387a41e802c189
jhonochoa <m8alemus@gmail.com>
2019-03-24 13:22:50 -0500

    Se implementa operaciones de consulta en 3 maneras

--- e-reservation/src/main/java/com/web/reserver/ereservation/negocio/repository/ClienteRepository.java
+++ e-reservation/src/main/java/com/web/reserver/ereservation/negocio/repository/ClienteRepository.java
@@ -1,12 +1,15 @@
 package com.web.reserver.ereservation.negocio.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.web.reserver.ereservation.modelo.Cliente;
 
 /**
  * Interface para definir las operaciones de base de datos ralacionadas 
- * con cliente
+ * con cliente. Ya la todos los metodos del CRUD los tiene la implemetación 
+ * de JpaRepository
  * 
  * @author sumel
  * 
@@ -14,4 +17,13 @@ import com.web.reserver.ereservation.modelo.Cliente;
  * */
 public interface ClienteRepository extends JpaRepository<Cliente, String>{ //JpaRepository<Entidad, Tipo de dato primario>
 
+	/**
+	 * Definición de metodo que busca clientes por su apellido
+	 * @author sumel
+	 * 
+	 * */
+	public List<Cliente> findByApellidoCli(String apellidoCli);
+	
+	public Cliente findByIdentification(String identificacionCli);
+	
 }
--- e-reservation/src/main/java/com/web/reserver/ereservation/negocio/repository/ReservaRepository.java
+++ e-reservation/src/main/java/com/web/reserver/ereservation/negocio/repository/ReservaRepository.java
@@ -1,6 +1,12 @@
 package com.web.reserver.ereservation.negocio.repository;
 
+
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.web.reserver.ereservation.modelo.Reserva;
 /**
@@ -9,5 +15,6 @@ import com.web.reserver.ereservation.modelo.Reserva;
  * */
 
 public interface ReservaRepository extends JpaRepository<Reserva, String>{
-
+	@Query("Select r from Reserva r where r.fechaIngresoRes =:fechaInicio and r.fechaSalidaRes =:fechaSalida")
+	public List<Reserva> find(@Param("fechaInicio") Date fechaInicio, @Param("fechaSalida")Date fechaSalida);	
 }
