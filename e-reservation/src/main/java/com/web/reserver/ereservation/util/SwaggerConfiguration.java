c230a8cb1a2171cfae2f9c6a0f555c654d2058b4
jhonochoa <m8alemus@gmail.com>
2019-03-24 15:47:57 -0500

    Se añade clase para manejar swagger

---
+++ e-reservation/src/main/java/com/web/reserver/ereservation/util/SwaggerConfiguration.java
@@ -0,0 +1,28 @@
+package com.web.reserver.ereservation.util;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Clase para configurar Swagger
+ * 
+ * @author sumel
+ * 
+ **/
+@Configurable
+@EnableSwagger2
+public class SwaggerConfiguration {
+	public Docket documentation() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+				.build();
+
+	}
+
+}
